3330a93b84d31f5bc3f9dcbdd4a7a9f67499fcec
Daniil Shivanov <pantyrevolution@gmail.com>
2022-02-08 22:40:54 +0300

    characters

--- Assets/Scripts/Unity/Views/Units/UnitView.cs
+++ Assets/Scripts/Unity/Views/Units/UnitView.cs
@@ -10,9 +10,28 @@ namespace GameUnity.Assets.Scripts.Unity.Views.Units
 {
     public class UnitView : ViewMonoBehaviour, IUnitView
     {
+        const float TIME_THRESHOLD = 0.1f;
+
+        [SerializeField] Animator _animator;
+        [SerializeField] string _runAnimaionKey;
+
+        private float _lastPositionUpdate;
+        private Vector3 _direction;
+
         public void SetPosition(FloatPoint position)
         {
-            transform.position = position.ToUnityVector(transform.position.y);
+            var newPosition = position.ToUnityVector(transform.position.y);
+            _lastPositionUpdate = Time.time;
+            _direction = transform.position - newPosition;
+            transform.position = newPosition;
+        }
+
+        protected void Update()
+        {
+            _animator.SetBool(_runAnimaionKey, (Time.time - _lastPositionUpdate < TIME_THRESHOLD));
+
+            if (_direction != Vector3.zero)
+                _animator.gameObject.transform.LookAt(_animator.gameObject.transform.position + _direction);
         }
     }
 }
