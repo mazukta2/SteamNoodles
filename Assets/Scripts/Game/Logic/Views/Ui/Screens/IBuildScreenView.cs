79c51e55a5da148ae6cde799328df5d837d3e4ca
mazukta2 <mazukta.mail@gmail.com>
2022-04-26 16:35:54 +0300

    wip

--- Assets/Scripts/Game/Logic/Views/Ui/Screens/IBuildScreenView.cs
+++ Assets/Scripts/Game/Logic/Views/Ui/Screens/IBuildScreenView.cs
@@ -1,4 +1,5 @@
-﻿using Game.Assets.Scripts.Game.Environment.Engine;
+﻿using Game.Assets.Scripts.Game.Environment.Creation;
+using Game.Assets.Scripts.Game.Environment.Engine;
 using Game.Assets.Scripts.Game.Logic.Presenters.Ui.Screens;
 using Game.Assets.Scripts.Game.Logic.Views.Common;
 
@@ -11,5 +12,7 @@ namespace Game.Assets.Scripts.Game.Logic.Views.Ui.Screens
         IText CurrentPoints { get; }
         IProgressBar PointsProgress { get; }
         BuildScreenPresenter Presenter { get; }
+        IViewContainer AdjacencyContainer { get; }
+        IViewPrefab AdjacencyPrefab { get; }
     }
 }
--- Assets/Scripts/Unity/Views/Ui/Screens/BuildScreenUnityView.cs
+++ Assets/Scripts/Unity/Views/Ui/Screens/BuildScreenUnityView.cs
@@ -1,4 +1,5 @@
-﻿using Game.Assets.Scripts.Game.Logic.Presenters.Ui.Screens;
+﻿using Game.Assets.Scripts.Game.Environment.Creation;
+using Game.Assets.Scripts.Game.Logic.Presenters.Ui.Screens;
 using Game.Assets.Scripts.Game.Logic.Views.Common;
 using Game.Assets.Scripts.Game.Logic.Views.Ui.Screens;
 using GameUnity.Assets.Scripts.Unity.Views.Ui.Common;
@@ -15,11 +16,16 @@ namespace Game.Assets.Scripts.Game.Unity.Views.Ui.Screens
         [SerializeField] TextMeshProUGUI _currentPoints;
         [SerializeField] Image _progress;
         [SerializeField] Image _additionalProgress;
+        [SerializeField] ContainerUnityView _adjacencyConteiner;
+        [SerializeField] PrototypeUnityView _adjacencyPrefab;
 
         public IButton CancelButton => _closeButton;
         public IWorldText Points => _additionalPoints;
         public IText CurrentPoints { get; private set; }
         public IProgressBar PointsProgress { get; private set; }
+
+        public IViewContainer AdjacencyContainer => _adjacencyConteiner;
+        public IViewPrefab AdjacencyPrefab => _adjacencyPrefab;
 
         protected override void PreAwake()
         {
