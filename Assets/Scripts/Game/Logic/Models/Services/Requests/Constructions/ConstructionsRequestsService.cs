e7bffd1ca3da1fc6d1cc2795ea699c041be6549b
Daniil Shivanov <pantyrevolution@gmail.com>
2022-06-13 15:59:15 +0300

    model to service

--- Assets/Scripts/Game/Logic/Models/Services/Requests/Constructions/ConstructionsRequestsService.cs
+++ Assets/Scripts/Game/Logic/Models/Services/Requests/Constructions/ConstructionsRequestsService.cs
@@ -1,4 +1,6 @@
-﻿using Game.Assets.Scripts.Game.Logic.Common.Core;
+﻿using Game.Assets.Scripts.Game.Environment.Creation;
+using Game.Assets.Scripts.Game.Environment.Engine;
+using Game.Assets.Scripts.Game.Logic.Common.Core;
 using Game.Assets.Scripts.Game.Logic.Common.Math;
 using Game.Assets.Scripts.Game.Logic.Common.Services;
 using Game.Assets.Scripts.Game.Logic.Models.Constructions;
@@ -95,5 +97,52 @@ namespace Game.Assets.Scripts.Game.Logic.Models.Services.Requests
             OnRemoved(obj);
         }
 
+        public class ConstructionModel : Disposable, IConstructionModel
+        {
+            public event Action OnDestroyed = delegate { };
+            public event Action OnExplostion = delegate { };
+            public event Action OnUpdate = delegate { };
+
+
+            private readonly Construction _construction;
+            private readonly ConstructionsRequestsService _service;
+            private readonly IAssets _assets;
+
+            public ConstructionModel(Construction construction, ConstructionsRequestsService service, IAssets assets)
+            {
+                _construction = construction;
+                _service = service;
+                _assets = assets;
+                _service.OnUpdate += HandleOnUpdate;
+                _service.OnRemoved += HandleOnRemoved;
+            }
+
+            protected override void DisposeInner()
+            {
+                _service.OnUpdate -= HandleOnUpdate;
+                _service.OnRemoved -= HandleOnRemoved;
+            }
+
+            public ConstructionScheme Scheme => _construction.Scheme;
+            public FieldRotation GetRotation() => _construction.Rotation;
+            public GameVector3 GetWorldPosition() => _service.GetWorldPosition(_construction);
+            public float GhostShrinkDistance => Scheme.GhostShrinkDistance;
+            public float GhostHalfShrinkDistance => Scheme.GhostHalfShrinkDistance;
+            public IViewPrefab GetModelAsset() => _assets.GetPrefab(Scheme.LevelViewPath);
+            public void Shake() => _service.Shake();
+            public float GetShrinkValue() => _service.GetShrink(this);
+
+            private void HandleOnUpdate()
+            {
+                OnUpdate();
+            }
+
+            private void HandleOnRemoved(Construction obj)
+            {
+                if (_construction.Id == obj.Id)
+                    OnExplostion();
+            }
+
+        }
     }
 }
