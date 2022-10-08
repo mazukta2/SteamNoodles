0599eb44b0eff9fbf68c8cbb2726b26e122bd37a
mazukta2 <mazukta.mail@gmail.com>
2022-10-08 20:55:44 +0600

    start menu

---
+++ Assets/Scripts/Game/Logic/Definitions/Levels/Starters/StartMenuLevel.cs
@@ -0,0 +1,19 @@
+﻿using Game.Assets.Scripts.Game.Logic.Definitions.Common;
+using Game.Assets.Scripts.Game.Logic.Models.Levels;
+using Game.Assets.Scripts.Game.Logic.Models.Session;
+using Game.Assets.Scripts.Game.Logic.Models.Time;
+
+namespace Game.Assets.Scripts.Game.Logic.Definitions.Levels.Starters
+{
+    public class StartMenuLevel : LevelStarter
+    {
+        public override ILevel CreateModel(LevelDefinition definition)
+        {
+            return new StartMenu(definition, IGameRandom.Default, IGameTime.Default, IGameDefinitions.Default);
+        }
+
+        public override void Start()
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Logic/Models/Levels/StartMenu.cs
@@ -0,0 +1,34 @@
+﻿using Game.Assets.Scripts.Game.Logic.Common.Core;
+using Game.Assets.Scripts.Game.Logic.Definitions;
+using Game.Assets.Scripts.Game.Logic.Definitions.Constructions;
+using Game.Assets.Scripts.Game.Logic.Definitions.Levels;
+using Game.Assets.Scripts.Game.Logic.Models.Building;
+using Game.Assets.Scripts.Game.Logic.Models.Constructions;
+using Game.Assets.Scripts.Game.Logic.Models.Customers;
+using Game.Assets.Scripts.Game.Logic.Models.Levels.Types;
+using Game.Assets.Scripts.Game.Logic.Models.Session;
+using Game.Assets.Scripts.Game.Logic.Models.Time;
+using Game.Assets.Scripts.Game.Logic.Models.Units;
+using System;
+using Game.Assets.Scripts.Game.Logic.Definitions.Common;
+using Game.Assets.Scripts.Game.Logic.Definitions.Customers;
+
+namespace Game.Assets.Scripts.Game.Logic.Models.Levels
+{
+    public class StartMenu : Disposable, ILevel
+    {
+
+        public StartMenu(LevelDefinition settings, IGameRandom random, IGameTime time, IGameDefinitions definitions)
+        {
+            Definition = settings;
+        }
+
+
+        public LevelDefinition Definition { get; set; }
+
+
+        protected override void DisposeInner()
+        {
+        }
+    }
+}
