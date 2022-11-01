3c9d9748c2a0e5e21e35cc7eb7fa526c34cfea32
mazukta2 <mazukta.mail@gmail.com>
2022-11-01 13:58:27 +0100

    timeline test

---
+++ Assets/Scripts/Unity/Views/Common/SceneTimelineAnimation.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Playables;
+
+[RequireComponent(typeof(PlayableDirector))]
+public class SceneTimelineAnimation : MonoBehaviour
+{
+    [SerializeField] string _tag;
+
+    private static List<SceneTimelineAnimation> _list = new List<SceneTimelineAnimation>();
+
+    protected void Awake()
+    {
+        _list.Add(this);
+    }
+
+    protected void OnDestroy()
+    {
+        _list.Remove(this);
+    }
+
+
+    public static void Play(string tag, string animationName)
+    {
+        foreach (var item in _list)
+        {
+            if (item._tag == tag)
+            {
+                //item.gameObject.GetComponent<PlayableDirector>();
+            }
+        }
+    }
+}
+
