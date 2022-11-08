12001460422411269d5b724f8f58de1be2d39d50
mazukta2 <mazukta.mail@gmail.com>
2022-11-08 13:44:00 +0100

    dialog localization

--- Assets/Scripts/Unity/Views/Common/Timeline/DialogueControlAsset.cs
+++ Assets/Scripts/Unity/Views/Common/Timeline/DialogueControlAsset.cs
@@ -1,4 +1,10 @@
 using System;
+using System.Windows.Forms.VisualStyles;
+using Game.Assets.Scripts.Game.Logic.Definitions.Common;
+using Game.Assets.Scripts.Game.Logic.Definitions.Languages;
+using Game.Assets.Scripts.Game.Logic.Presenters.Localization;
+using GameUnity.Assets.Scripts.Unity.Engine.Definitions;
+using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.Playables;
 
@@ -7,6 +13,10 @@ namespace GameUnity.Unity.Views.Common.Timeline
     public class DialogueControlAsset : PlayableAsset
     {
         [SerializeField] private Line[] _lines;
+        
+        [SerializeField] [ReadOnly]
+        [ResizableTextArea]
+        private string _text;
         
         public override Playable CreatePlayable (PlayableGraph graph, GameObject owner)
         {
@@ -21,6 +31,21 @@ namespace GameUnity.Unity.Views.Common.Timeline
 
             return playable;
         }
+
+        public void OnValidate()
+        {
+            var defs = new GameDefinitions(new UnityDefinitions());
+            var localizationManager = new LocalizationManager(defs, "English");
+            _text = "";
+            for (var index = 0; index < _lines.Length; index++)
+            {
+                var line = _lines[index];
+                var text = localizationManager.Get(line.Character) + ": '" + localizationManager.Get(line.Tag) + "'" +
+                        System.Environment.NewLine;
+                _text += text;
+            }
+        }
+        
         
         [Serializable]
         public struct Line
